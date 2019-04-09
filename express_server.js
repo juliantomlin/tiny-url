bae7568a8e2cdae89ff0bf25c5a199f7a2a7e4d2
Julian <julian@juliantomlindesign.com>
2019-04-09 23:11:18 +0000

    added new views and random string gen

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 const urlDatabase = {
@@ -18,15 +20,36 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new.ejs")
+})
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars)
 })
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+})
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+function generateRandomString () {
+  while (true){
+    let r = Math.random().toString(36).substring(7);
+    if (r.length === 6) {
+      return r
+    }
+  }
+}
